561a5df0522469a2957f3e4d352aa338ef66c5e7
cd1412 <chiduong1412@gmail.com>
2021-10-01 22:21:10 -0500

    commit tree 0e893d1b0b7ceff6ded840b546a9e4a5b0eb7355

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,70 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    #You can also do WIN_COMBINATIONS.any? do |win_combination|
+    # win_combination.all? do |win_index|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+     position_1 = board[win_index_1]
+     position_2 = board[win_index_2]
+     position_3 = board[win_index_3]
+
+     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      # You have to include && position_taken?(board, win_index_1)
+      return win_combination
+     end
+    end
+     return false
+    end
+  
+
+
+def full?(board)
+  board.all? do |index|
+  index == "X" or index == "O"
+  end
+end
+
+def draw?(board)
+if won?(board) == false && full?(board) == true
+  # Another way to write it is !won?(board) meaning won?(board) == false
+  true
+elsif full?(board) == false && won?(board) == false
+  false
+else won?(board)
+  false
+end
+end
+
+def over?(board)
+if draw?(board) == true or won?(board) or full?(board)
+  # The line above will only pass with this order of draw, won, and then full. 
+  # When the order was won, full, and draw, it did not pass
+  true
+else
+  false
+end
+end
+
+def winner(board)
+  if won?(board)
+    # When you wrote won?(board) == true, it did not pass
+    return board[won?(board)[0]]
+  end
+end
